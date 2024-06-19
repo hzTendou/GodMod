44a8dc56f1c9e16767ee65f57525ea44273dce7e
Famil Hasanov <80038372+hzTendou@users.noreply.github.com>
2024-06-19 17:11:44 +0400

    Add files via upload

---
+++ scripts/classes/error/command.js
@@ -0,0 +1,14 @@
+import { world } from "@minecraft/server"
+
+export default class CommandError {
+  constructor({ message, player }) {
+    this.message = message;
+    this.player = player;
+    this.sendErrorMessage();
+  }
+  
+  sendErrorMessage() {
+    const command = `tellraw "${this.player}" ${JSON.stringify({ rawtext: [ { text: this.message } ] })}`
+    world.getDimension("overworld").runCommandAsync(command)
+  }
+}
